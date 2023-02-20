15666a1bd497a649f828dda9f1235ed555dd0f36
NyeletiStrela <nyeleti.strela@gmail.com>
2023-02-20 05:02:10 -0800

    output a-z axcept e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: output a-z except e and q
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		if (n != 'e' && n != 'q')
+			putchar(n);
+
+		putchar('\n');
+		return (0);
+}
