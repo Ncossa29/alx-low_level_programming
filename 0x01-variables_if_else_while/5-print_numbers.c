190138246b032a522e3980d6aecd8f38a1db3b4b
NyeletiStrela <nyeleti.strela@gmail.com>
2023-02-20 07:04:10 -0800

    number 1-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: print 0-9
+ * Return always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	if (n = 0; n < 10; n++)
+		printf("%d", n);
+
+	printf("\n");
+		return (0);
+}
