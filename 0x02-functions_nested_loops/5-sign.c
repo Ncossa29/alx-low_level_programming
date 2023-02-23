73e3336000ac32b6f87fc6540d8717eb09be717c
NyeletiStrela <nyeleti.strela@gmail.com>
2023-02-22 16:40:04 -0800

    value of an int

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_sigh - prototype
+ * description: output sigh of number
+ * @n: int to be checked
+ * Return: retuns 1 if number = to zero
+ */
+int print_sigh(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - value of an int
+ * @n: int to be checked
+ * Return: the value of the int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (n);
+}
