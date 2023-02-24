66ebda0ebaa22e219a9147995ff7765fba339668
NyeletiStrela <nyeleti.strela@gmail.com>
2023-02-24 12:11:48 -0800

    pritn 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers -  checks for checks for a digit (0 through 9).
- *
+ * print_numbers - prototype 
+ * checks for checks for a digit (0 through 9).
  * Return: Always 0.
  */
 void print_numbers(void)
