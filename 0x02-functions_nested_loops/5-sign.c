65be762df247d122e2259b5a4510cd43b7f29d39
NyeletiStrela <nyeleti.strela@gmail.com>
2023-02-24 04:16:20 -0800

    pritn sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,15 +1,25 @@
 #include "main.h"
 /**
- * print_sigh - prototype
+ * print_sign - prototype
  * description: output sigh of number
  * @n: int to be checked
  * Return: retuns 1 if number = to zero
  */
-int print_sigh(int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
