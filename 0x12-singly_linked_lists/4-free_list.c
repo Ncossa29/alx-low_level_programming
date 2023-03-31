7ce1f2049fbe8e35c9f1af16dcf2542e6d0771ac
NyeletiStrela <nyeleti.strela@gmail.com>
2023-03-31 04:17:51 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <list.h>
+
+/**
+ * void free_list(list_t *head) - prototype
+ * @head: list_t to be freed
+ */
+
+void free_list(list_t *head);
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
