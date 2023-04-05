a4a2325c3cbb0f95f7a0b00490060a9740ec22c1
NyeletiStrela <nyeleti.strela@gmail.com>
2023-04-05 11:25:43 -0700

    function that returns the number of elements in a linked listint_t list.

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a linked list
+ * add_nodeint - 
+ * discription - adds a new node at the beginning the linked list
  * @head: pointer to the first node in the list
- * @n: data to insert in that new node
- *
+ * @n: data to to be inserted on new node
  * Return: pointer to the new node, or NULL if it fails
  */
 listint_t *add_nodeint(listint_t **head, const int n)
