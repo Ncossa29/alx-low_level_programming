13840b5e88b9cca280def5b64700d54b9dee546a
NyeletiStrela <nyeleti.strela@gmail.com>
2023-04-05 11:43:56 -0700

    sets head to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a linked list
+ * free_listint2 - prototype 
+ * dicrpetion - frees a linked list but sets *head to NULL
  * @head: pointer to the listint_t list to be freed
  */
 void free_listint2(listint_t **head)
