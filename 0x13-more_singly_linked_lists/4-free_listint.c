e743cfd67fb13a92a9861fa09992eada5f00f6f0
NyeletiStrela <nyeleti.strela@gmail.com>
2023-04-05 11:40:16 -0700

    free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listint - frees a linked list
+ * free_listint - prototype
+ * discritption - free linked list
  * @head: listint_t list to be freed
  */
 void free_listint(listint_t *head)
