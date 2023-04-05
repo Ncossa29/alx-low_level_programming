1f9bf5baa936e717ba4cdac16fae4ae343c348c2
NyeletiStrela <nyeleti.strela@gmail.com>
2023-04-05 11:56:05 -0700

    add node index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - returns the node at a certain index in a linked list
+ * get_nodeint_at_index - prototype
+ * returns the node at an index in a linked list
  * @head: first node in the linked list
- * @index: index of the node to return
- *
- * Return: pointer to the node we're looking for, or NULL
+ * @index: index of the node
+ * Return: pointer to the node, or NULL
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
