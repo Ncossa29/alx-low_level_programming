34a9e9fdc7a3cf9295384bc20f606b05c54eb7fe
Ncossa29 <nyeleti.strela@gmail.com>
2023-06-08 04:54:04 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * _strlen_recursion - funtion prototype
+ * @s: input
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s)
+	{
+		n++;
+		n += _strlen_recursion(s +1);
+	}
+	return(n);
+
+}
