2705a2fa3cfa12667c0669fe3c50310b7d30ebda
Ncossa29 <nyeleti.strela@gmail.com>
2023-06-08 05:35:34 -0700

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,16 +1,16 @@
 #include"main.h"
 
 /**
- * int factorial - function prototype
- * @n: the variable
- * return: output -1 if there is an error
+ * factorial - function prototype
+ * @n:the variable
+ * Return: output -1 if there is an error
  */
 
 int factorial(int n)
 {
-	if (n<0)
+	if (n < 0)
 		return (-1);
-	if (n==0)
+	if (n == 0)
 		return(1);
 
 	return (n*factorial(n - 1));
