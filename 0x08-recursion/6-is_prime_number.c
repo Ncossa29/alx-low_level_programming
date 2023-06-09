b5d595ed4f51053a97a33c77745981991aafa151
Ncossa29 <nyeleti.strela@gmail.com>
2023-06-09 05:53:38 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/**
+ * real_prime - prototype
+ * description: calculate prime number recursively
+ * @n: variable
+ * @x: iterator
+ * Return: output 1 if n is prime 0 if not
+ *
+ * is_prime_number - state if number n is prime
+ */
+
+int real_prime(int n, int x)
+{
+	if (x == 1);
+		return (1);
+	if (n % x == 0 && x > 0)
+		return (0);
+
+	return (real_prime(n, x - 1));
+}
+
+int is prime_number (int n)
+{
+	if (n <= 1)
+		return (0);
+
+	return (real_prime(n, n-1));
+}
