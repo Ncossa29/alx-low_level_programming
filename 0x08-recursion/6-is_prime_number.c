48e8b69e21541b76c67c70269713ad3f78a5650e
Ncossa29 <nyeleti.strela@gmail.com>
2023-06-09 05:57:56 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@
 
 int real_prime(int n, int x)
 {
-	if (x == 1);
+	if (x == 1)
 		return (1);
 	if (n % x == 0 && x > 0)
 		return (0);
@@ -20,7 +20,7 @@ int real_prime(int n, int x)
 	return (real_prime(n, x - 1));
 }
 
-int is prime_number (int n)
+int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
