e522042d42f6822f3df877472b5acd90e90c9b61
Ncossa29 <nyeleti.strela@gmail.com>
2023-06-12 05:25:18 -0700

    library

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,1 +1,11 @@
-int _isalpha(int c);
+#include "main.h"
+/**
+ * _isalpha - code chekcs if upper case
+ * @c: is to be checked
+ * description: code will check for uppercase char
+ * Return: if is upper case 1 else 0
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,1 +1,14 @@
-int _abs(int n);
+#include "main.h"
+/**
+ * _abs - value of an int
+ * @n: int to be checked
+ * Return: the value of the int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
