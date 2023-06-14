fdf8635c0d81ca0d4b523f96c1d22420814547e5
Ncossa29 <nyeleti.strela@gmail.com>
2023-06-14 02:41:57 -0700

    task 0

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include<stdlib.h>
+#include"main.h"
+
+/**
+ * _strdup - prototype
+ * Description: copy str to new memory space location
+ * @str: pointer variable
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+        char *a;
+        int j, k = 0;
+
+        if (str == NULL)
+                return (NULL);
+        j = 0;
+        while (str[j] != '\0')
+                j++;
+
+        a = malloc(sizeof(char) * (j + 1));
+
+        if (a == NULL)
+                return (NULL);
+        for (k = 0; str[k]; k++)
+                a[k] = str[k];
+
+        return (a);
+}
