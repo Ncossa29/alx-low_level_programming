464d769d2f44d9646bfb9fb8c760cf3bfe0b66fb
Ncossa29 <nyeleti.strela@gmail.com>
2023-06-29 02:02:14 -0700

    advance

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,13 +2,11 @@
 
 /**
  * real_prime - prototype
- * description: calculate prime number recursively
+ * calculate prime number recursively
  * @n: variable
  * @x: iterator
- * Return: output 1 if n is prime 0 if not
- *
- * is_prime_number - function prototype
- * descrption: state if number n is prime
+ * is_prime_number -  n is prime?
+ * Return: output 1 if n is a prime 0 if not
  */
 
 int real_prime(int n, int x)
@@ -20,6 +18,9 @@ int real_prime(int n, int x)
 
 	return (real_prime(n, x - 1));
 }
+/**
+ * function state if n is a prime
+ */
 
 int is_prime_number(int n)
 {
