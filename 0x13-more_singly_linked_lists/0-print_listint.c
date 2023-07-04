5fbdf40e49fc47726818c89757c5fe76d3f9a053
Ncossa29 <nyeleti.strela@gmail.com>
2023-07-04 07:27:19 -0700

    task0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements in a linked list
+ * @t: linked list of listint_t 
+ * Return: number of nodes print
+ */
+size_t print_listint(const listint_t *h)
+{
+ size_t num = 0;
+
+ while (t)
+
+ {
+ printf("%d\n", t->n);
+ num++;
+ t = t->next;
+ }
+
+ return (num);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+more tasks regarding single linked lists
+ a total of 13 projects and 1 header file
