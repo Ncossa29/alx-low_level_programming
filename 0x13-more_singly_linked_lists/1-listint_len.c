9a7b224a6b9d81a54183b26d08a5972f00531c82
Ncossa29 <nyeleti.strela@gmail.com>
2023-07-04 17:58:52 -0700

    task1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,18 +3,18 @@
 /**
  * listint_len - returns the number of elements in a linked lists
  * @h: linked list to be checked
- * Return: number of nodes
+ * Return:the number of nodes
  */
 
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0;
+	size_t nd = 0;
 
 	while (h)
 	{
-	num++;
+	nd++;
 	h = h->next;
 	}
 
-	return (num);
+	return (nd);
 }
