ad11cf34f9b7551969136f63b1745e68ae25a7d1
Ncossa29 <nyeleti.strela@gmail.com>
2023-07-14 03:19:11 -0700

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	p = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	w = write(p, text_content, len);
 
 	if (p == -1 || w == -1)
 		return (-1);
