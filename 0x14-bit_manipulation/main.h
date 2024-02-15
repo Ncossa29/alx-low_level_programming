a255432a75fd0b4db8d2960af7e55499be8ee58d
Ncossa29 <nyeleti.strela@gmail.com>
2024-02-15 06:30:11 -0800

    header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,6 +8,7 @@ int get_bit(unsigned long int n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _atoi(const char *s);
 int get_endianness(void);
+unsigned int binary_to_uint(const char *b);
 
 
 #endif
