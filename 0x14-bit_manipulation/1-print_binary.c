db404ed70ee440f46377f8ad28891f70aa96350a
Ncossa29 <nyeleti.strela@gmail.com>
2024-02-16 05:08:56 -0800

    task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_binary - convert a decimal number to binary
+ * @n: conveted binary number
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i;
+	int p = 0;
+	unsigned long int curr;
+
+	for (i = 63; i >= 0; i--)
+	{
+		curr = n >> i;
+
+		if (curr & 1)
+		{
+			_putchar('1');
+			p++;
+		}
+		else if (p)
+			_putchar('0');
+	}
+	if (!p)
+		_putchar('0');
+}
