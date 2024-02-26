b1c0ce2c7d9c0a64c85ba4ca8ebd0a0428891f96
Ncossa29 <nyeleti.strela@gmail.com>
2024-02-26 02:03:58 -0800

    task5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,7 +5,7 @@
  * would need to flip to get from one number to another
  * @n: 1st number
  * @m: 2nd number
- * Return: number of changed bits 
+ * Return: number of changed bits
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
